1ffcca6ad6328abed2fb5482f98546381504f44b
Solange <solangedesjardins@gmail.com>
2022-08-30 13:40:08 -0400

    bug fixes

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -38,6 +38,7 @@ export default function Dictionary() {
         </form>
         <hr />
       </div>
+
       <Results results={results} />
     </div>
   );
--- src/Results.js
+++ src/Results.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Meaning from "./meaning";
 
 export default function Results(props) {
